10a8768c9eb0fde12d795d14436421db4bc7c31b
xchall <macsnebyvalov@yandex.ru>
2025-03-29 08:51:10 +0300

    Lab5.4 Сумма цифр, добавил хвостовую реккурсию

--- Функции высших порядков Рекурсия/work_with_numbers.fs
+++ Функции высших порядков Рекурсия/work_with_numbers.fs
@@ -5,8 +5,16 @@ let rec sum_cifr x =
     if x / 10 = 0 then x%10 //дно реккурсии
     else (x % 10) + sum_cifr (x / 10)  
 
+//хвостовая реккурсия 
+let sum_cifr_hvost x =
+    let rec loop num acc = 
+        if num / 10 = 0 then acc + (num%10) 
+        else loop (num / 10) (acc + (num % 10)) 
+    loop x 0
+
 let main () = 
     Console.Write("Введите число для подсчета суммы цифр: ")
     let num = Console.ReadLine() |> int
     Console.WriteLine($"Сумма цифр (найдено при помощи реккурсии вверх) {sum_cifr num}")
+    Console.WriteLine($"Сумма цифр (найдено при помощи хвостовой реккурсии) {sum_cifr_hvost num}")
 main () 
