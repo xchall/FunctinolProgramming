6e4f65346b821c760c186d470e0170cdf24e909f
xchall <macsnebyvalov@yandex.ru>
2025-04-05 07:49:13 +0300

    Lab5 добавлены задания 7-10 с рекурсиями

--- Функции высших порядков Рекурсия/work_with_numbers_3_func.fs
+++ Функции высших порядков Рекурсия/work_with_numbers_3_func.fs
@@ -1,61 +1,66 @@
 open System
-//вариант 5 для задания 16
-//1
-let amountDeviders num =
-    let rec loop n (f: int-> int-> bool) acc (p:int->bool) = 
-        match n with
-        | 0 -> acc
-        | _ -> 
-            let newAcc = if f num n then (if p n then (acc+1) else acc) else acc
-            loop (n-1) f newAcc p
-    loop num (fun x y -> if x % y =0 then true else false) 0 (fun x -> if x % 3 =0 then false else true)
 
-//2 
-let minNechCifr num  = 
-    let rec loop n (f: int-> int-> int) acc (p:int->bool) = 
-        match n with
-        | 0 -> acc
-        | _ -> 
-            let nAcc = (if p (n%10) then (f acc (n%10)) else acc)
-            loop (n/10) f nAcc p
-    loop num (fun x y -> (if x < y then x else y)) 10 (fun x -> if x%2<>0 then true else false)
+//реккурсия вверх
+let rec sum_cifr x = 
+    if x / 10 = 0 then x%10 //дно реккурсии
+    else (x % 10) + sum_cifr (x / 10)  
 
-//3
+//хвостовая реккурсия 
 let sum_cifr_hvost x =
     let rec loop num acc = 
         if num / 10 = 0 then acc + (num%10) 
         else loop (num / 10) (acc + (num % 10)) 
     loop x 0
 
-let mult_cifr_hvost x =
-    let rec loop num acc = 
-        if num / 10 = 0 then acc * (num%10) 
-        else loop (num / 10) (acc * (num % 10)) 
-    loop x 1
+let rec amount_deviders (x:int) (del:int) =
+    match del with
+    | 0 -> 0
+    | _ when ((x % del) = 0) -> (1 + (amount_deviders x (del-1)))
+    | _ -> amount_deviders x (del-1)
 
-let vzaimnoProst (num1:int) (num2:int) = 
-    let rec loop x =
-        match x with
-        | 1 -> true
-        | _ -> (if num1 % x = 0 then (if num2 % x = 0 then false else loop (x-1)) else loop (x-1))
-    loop num1 
+let amount (x:int) = 
+    amount_deviders x x
 
-let sumDeviders num =
-    let sumCifr = (sum_cifr_hvost num)
-    let multCifr = (mult_cifr_hvost num)
-    let rec loop x acc (f:int->int->bool)=
-        match x with
-        | 0 -> acc
-        | _ -> 
-            let newAcc = if num % x= 0 then (if (f x sumCifr) then (if (f x multCifr) then acc else (acc+x)) else acc) else acc  
-            loop (x-1) newAcc f
-    loop num 0 vzaimnoProst
+let amount_deviders_hvost (x:int) =
+    let rec loop num del acc = 
+         match del with
+        | 0 -> 0
+        | _ when ((num % del) = 0) -> (loop num (del-1) acc+1)
+        | _ -> loop num (del-1) acc
+    loop x x 0
 
+
+let dificult_func (flag: bool) = 
+    match flag with
+    | true -> sum_cifr_hvost 
+    | false -> amount_deviders_hvost
+
+//7,8
+let rec cifirFold (num:int) (f:int->int->int) (acc:int) =
+    match num with
+    | 0 -> acc
+    | _ -> 
+        let numAcc = f acc (num % 10)
+        cifirFold (num/10) f numAcc 
+//9-10
+let rec cifirFoldSecond (num:int) (f:int->int->int) (acc:int) (p:int->bool) =
+    match num with
+    | 0 -> acc
+    | _ -> 
+        let numAcc = (if p (num%10) then (f acc (num%10)) else acc)
+        cifirFoldSecond (num/10) f acc p
+    
 let main () = 
-    let ans1 = amountDeviders 15
-    Console.WriteLine($"Количество делителей числа, не делящихся на 3 = {ans1}")
-    let ans2 = minNechCifr 667372
-    Console.WriteLine($"Минимальная нечетная цифра (если ответ = 10, то нечетных цифр нет) = {ans2}")
-    let ans3 = sumDeviders 76
-    Console.WriteLine($"Cумма всех делителей числа, взаимно простых с суммой \nцифр числа и не взаимно простых с произведением цифр числа = {ans3}")
-main()
+    //Console.Write("Введите число для подсчета суммы цифр: ")
+    //let num = Console.ReadLine() |> int
+    //Console.WriteLine($"Количсетво делителей числа (реккурсия вверх) {amount num }")
+    //Console.WriteLine($"Количсетво делителей числа (хвостовая реккурсия) {amount_deviders_hvost num }")
+    //let func = dificult_func false
+    //Console.WriteLine($" {func num}")
+    //Console.WriteLine($"Сумма цифр (найдено при помощи реккурсии вверх) {sum_cifr num}")
+   // Console.WriteLine($"Сумма цифр (найдено при помощи хвостовой реккурсии) {sum_cifr_hvost num}")
+    Console.WriteLine($"Сумма цифр {cifirFold 781 (fun x y -> (x+y)) 0}")
+    Console.WriteLine($"Произведение цифр {cifirFold 781 (fun x y -> x*y) 1}")
+    Console.WriteLine($"Минимум {cifirFold 781 (fun x y -> if x < y then x else y) 10}")
+    Console.WriteLine($"Максимум {cifirFold 781 (fun x y -> if x > y then x else y) 0}")
+main () 
