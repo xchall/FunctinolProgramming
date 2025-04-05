edca4e03e2ab91307a0954318db41f05402c0fa2
xchall <macsnebyvalov@yandex.ru>
2025-04-05 10:23:18 +0300

    Lab5.15 Добавлена функция обхода с условием
    На основе написанных функций построить функции обход
    взаимнопростых с условием. Протестировать.
    Используется предикат.

--- Функции высших порядков Рекурсия/obhod_vzaimprost.fs
+++ Функции высших порядков Рекурсия/obhod_vzaimprost.fs
@@ -15,6 +15,13 @@ let obhod num (f: int -> int -> int) init =
     loop 1 init
 let euler num = 
     obhod num (fun x y -> y + 1) 0
+let obhodWithCondition num (f: int -> int -> int) init (condition: int -> bool) =
+    let rec loop x acc = 
+        match x with 
+        | _ when num = x -> acc
+        | _ -> (if (vzaimnoProst x num) = true then (if (condition x) then (f x acc) |> loop (x+1) else (loop (x+1) acc)) else (loop (x+1) acc))
+    loop 1 init
+
 let main () = 
     let ans1 = obhod 8 (fun x y -> x + y) 0
     Console.WriteLine($"{ans1}") 
@@ -22,4 +29,6 @@ let main () =
     Console.WriteLine($"{ans2}") 
     let ans3 = euler 27
     Console.WriteLine($"Количество взаимнпростых с 27 равно {ans3}") 
+    let ans4 = obhodWithCondition 35 (fun x y -> y + 1) 0 (fun x -> (x % 3 = 0)) 
+    Console.WriteLine($"Количество взаимнпростых с 35 равно, которые делятся на 3 равно {ans4}") 
 main()
