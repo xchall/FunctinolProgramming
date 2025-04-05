e2cd1482df20164b74d4e8e61a5fefb8bf6d1f54
xchall <macsnebyvalov@yandex.ru>
2025-04-05 10:02:10 +0300

    Lab5.14 Добавлена функция Эйлера
    Построить на её основе функции для вычисления числа Эйлера.

--- Функции высших порядков Рекурсия/obhod_vzaimprost.fs
+++ Функции высших порядков Рекурсия/obhod_vzaimprost.fs
@@ -13,11 +13,13 @@ let obhod num (f: int -> int -> int) init =
         | _ when num = x -> acc
         | _ -> (if (vzaimnoProst x num) = true then ((f x acc) |> loop (x+1))  else (loop (x+1) acc))
     loop 1 init
-
+let euler num = 
+    obhod num (fun x y -> y + 1) 0
 let main () = 
     let ans1 = obhod 8 (fun x y -> x + y) 0
     Console.WriteLine($"{ans1}") 
     let ans2 = obhod 17 (fun x y -> x + y) 0
     Console.WriteLine($"{ans2}") 
-
+    let ans3 = euler 27
+    Console.WriteLine($"Количество взаимнпростых с 27 равно {ans3}") 
 main()
