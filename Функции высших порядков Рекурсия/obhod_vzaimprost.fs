ae06bc0b04470c69cfb30679c3d765dc4a52568f
xchall <macsnebyvalov@yandex.ru>
2025-04-05 09:48:42 +0300

    Lab5.13 Обход по взаимнопростым числам
    Написать функцию обход взаимно простых компонентов
    числа, которая выполняет операции над числами, взаимно простыми с
    данным, принимает три аргумента, число, функция (например, сумма,
    произведение, минимум, максимум, количество) и инициализирующее
    значение. Функция должна иметь два Int аргумента и возвращать Int.

---
+++ Функции высших порядков Рекурсия/obhod_vzaimprost.fs
@@ -0,0 +1,23 @@
+open System
+
+let vzaimnoProst (num1:int) (num2:int) = 
+    let rec loop x =
+        match x with
+        | 1 -> true
+        | _ -> (if num1 % x = 0 then (if num2 % x = 0 then false else loop (x-1)) else loop (x-1))
+    loop num1 
+
+let obhod num (f: int -> int -> int) init = 
+    let rec loop x acc = 
+        match x with 
+        | _ when num = x -> acc
+        | _ -> (if (vzaimnoProst x num) = true then ((f x acc) |> loop (x+1))  else (loop (x+1) acc))
+    loop 1 init
+
+let main () = 
+    let ans1 = obhod 8 (fun x y -> x + y) 0
+    Console.WriteLine($"{ans1}") 
+    let ans2 = obhod 17 (fun x y -> x + y) 0
+    Console.WriteLine($"{ans2}") 
+
+main()
