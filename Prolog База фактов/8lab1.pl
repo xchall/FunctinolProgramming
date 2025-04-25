f14035dc4617aecacd2baf0e1674f0a7d019d7c2
xchall <macsnebyvalov@yandex.ru>
2025-04-25 23:51:14 +0300

    Lab8.1 предикаты mother father brother

---
+++ Prolog База фактов/8lab1.pl
@@ -0,0 +1,19 @@
+man :- man(X), print(X), nl, fail.
+
+children(X):- parent(X, Y), print(Y), nl, fail.
+mother(X, Y) :- woman(X), parent(X,Y).
+mother(X) :- mother(Y, X), print(Y).
+
+father(X, Y) :- man(X), parent(X,Y).
+
+brother(X,Y) :- man(X), man(Y), X \= Y, (mother(Z,X), mother(Z,Y), father(G,X), father(G,Y)).
+brothers(X) :- brother(X,Y), print(Y), nl, fail.
+
+sister(X,Y) :- woman(X), woman(Y), X \= Y, (mother(Z,X), mother(Z,Y), father(G,X), father(G,Y)).
+sister(X) :- sister(X,Y), print(Y), nl, fail.
+
+b_and_s(X,Y) :- (woman(X), man(Y); man(X), woman(Y)), X \= Y, (mother(Z,X), mother(Z,Y), father(G,X), father(G,Y)).
+b_and_s(X) :- sister(X,Y), print(Y), nl, fail.
+
+b_s(X,Y) :- sister(X,Y); brother(X,Y); b_and_s(X,Y).
+b_s(X) :- b_s(X,Y), print(Y), nl, fail.
