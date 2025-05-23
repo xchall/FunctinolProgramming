0a87a507724591e8b16f80ccf5b79095ab2b49ec
xchall <macsnebyvalov@yandex.ru>
2025-05-24 01:09:57 +0300

    IndTask1 FindAnsSameDifferences
    FindAnsSameDifferences с использованием списка
    FindAnsSameDifferences2 с использованием массива, работает гораздо быстрее так как увеличение на 1 в накапливающем массиве происходит за O(1), для списка за O(N)

--- SameDifferences.fs
+++ SameDifferences.fs
@@ -18,47 +18,69 @@ let countXYZ (list:int List) =
     match squares with
     | head :: tail -> head - (List.reduce (fun acc x -> acc + x) tail) 
 
-let FindAnsSameDifferences current_num =
-    //fl - нужно, чтобы остановится
-    //acc - накапливаем количество решений для current_num
-    let rec loop (acc:int) (num:int) (st:int) (fl:int) = //начинаем с шага 1 в прогрессии, с первого эемента = 1
-        let progrList = AryfmProgrOf3 num st
-        let result =  countXYZ progrList
-        //Console.WriteLine($"Арифметическая прогрессиия {progrList}")
-        match result with
-        | _ when result = current_num -> //нашли одно решение
-                                        Console.WriteLine($"{result} = ")
-                                        let newAcc = acc + 1
-                                        let newNum = num + 1    
-                                        loop newAcc newNum 1 0
+//реккурсия вниз
+let updateByIndex (index:int) (list:int List) = 
+    let rec loop curPos spis resList=
+        match spis with
+        |[]-> List.rev resList
+        |head::tail -> 
+                    if curPos = index then
+                        let newHead = head + 1
+                        (List.rev (newHead :: resList)) @ tail // дно рекурсии, обязаны дойти до index
+                    else 
+                        let newCurPos = curPos + 1
+                        let newResList = head :: resList
+                        loop newCurPos tail newResList
+                        
+    loop 1 list [] //важно! индексация 
 
-        | _ when result < current_num -> 
-                                        //Console.WriteLine($"<")
-                                        if fl = 10000000 then 
-                                            Console.WriteLine($"{result} < ")
-                                            acc
-                                        else
-                                            let newStep = st + 1
-                                            let newFl = fl+1
-                                            loop acc num newStep newFl
-        | _ when result > current_num-> 
 
-                                        if fl = 10000000 then 
-                                            Console.WriteLine($"{result} > ")
-                                            acc //дно рекурсии
-                                        else 
-                                            //Console.WriteLine($"{num} ")
-                                            let newNum = num + 1
-                                            let newFl = fl+1
-                                            loop acc newNum 1 newFl
+let FindAnsSameDifferences current_limit =
+//n = u* v
+    let rec loop u v acc =
+        let p = 3*v - u
+        match p, v, u with 
+        |_,_,_ when (p > 0 && p % 4 = 0) && (u*v <= current_limit) -> 
+                                let newAcc = updateByIndex (u * v) acc
+                                loop u (v + 1) newAcc
+        |_,_,_ when (u*v <= current_limit) -> 
+                                loop u (v + 1) acc
+        |_,_,_ when (u < current_limit) -> 
+                                loop (u + 1) 1 acc
+        |_,_,_  -> acc //дно
 
-    loop 0 2 1 0
+    let zeros = List.init (current_limit+1) (fun _ -> 0)
+    loop 1 1 zeros
 
+let FindAnsSameDifferences2 current_limit =
+    let acc = Array.zeroCreate (current_limit + 1) //массив вместо списка
+    
+    let rec loop u v =
+        let p = 3*v - u
+        match p, v, u with 
+        |_,_,_ when (p > 0 && p % 4 = 0) && (u*v <= current_limit) -> 
+                                let n = u * v
+                                acc.[n] <- acc.[n] + 1
+                                loop u (v + 1) 
+        |_,_,_ when (u*v <= current_limit) -> 
+                                loop u (v + 1) 
+        |_,_,_ when (u < current_limit) -> 
+                                loop (u + 1) 1
+        |_,_,_  -> acc //дно
+     
+    loop 1 1
 
 [<EntryPoint>]
 let main argv =
-    let numbersForResearch = [1155..1600]
-    let answer = 
-            FindAnsSameDifferences 1155
-    Console.WriteLine($"{answer}")
+    let answer1 = 
+            FindAnsSameDifferences 20000 
+            |> List.filter (fun x -> x = 10) 
+            |> List.length
+    Console.WriteLine($"{answer1}")
+    let answer2 = 
+        FindAnsSameDifferences2 20000
+        |> Array.filter (fun x -> x = 10) 
+        |> Array.length
+    Console.WriteLine($"{answer2}")
+
     0
